c67c47e7b3272389e39047b304dd12759091eb1f
GenuinoVitto <vittogenuino@gmail.com>
2024-08-11 11:26:33 +0800

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,112 +1,128 @@
 import Image from "next/image";
+import Link from 'next/link';
+import {Card, CardHeader, CardBody, CardFooter, Divider} from "@nextui-org/react";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-slate-900 flex min-h-screen flex-row items-start p-24">
+      <div id="profile" className="fixed flex flex-col w-80">
+        <p className="text-4xl">Vitto Genuino</p>
+        <p className="py-2">Software Engineer</p>
+        <p className="py-2">What do I do?</p>
+
+        <nav className="bg-slate-900 flex flex-col pt-12">
+            <ul className="wsm:flex justify-evenly">
+              <li className="">
+                <Link href="/" className="hover:animate-pulse ">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link href="#experience">Experience</Link>
+              </li>
+              <li>
+                <Link href="#projects">Projects</Link>
+              </li>
+              <li>
+                <Link href="#contributions">Contributions</Link>
+              </li>
+            </ul>
+          </nav>
+      </div>
+
+      <div id="main" className="flex flex-col ml-80 w-full">
+        <div id="about" className="pb-40">
+          I'm a 5th year college student taking up a Bachelor's Degree in Computer Science with a Major in Software Technology.
+          <br></br>
+          <br></br>
+          My main focus these days is <strong>Data Engineering</strong> and <strong>Software Architecture</strong>.
+          <br></br>
+          <br></br>
+          When I'm not on my computer, I'm usually playing Golf, working out in the Gym, and Running.
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div id="experience" className="pb-40">
+          <p>Experience</p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {/* LENA */}
+          <Card className="max-w-full mt-10">
+            <CardHeader className="flex gap-3">
+              <Image
+                alt="nextui logo"
+                height={40}
+                radius="sm"
+                src="/lena.jpg"
+                width={40}
+              />
+              <div className="flex flex-col">
+                <p className="text-md">Data Engineer Intern</p>
+                <p className="text-small text-default-500">
+                    Twelve Baskets Analytics, Inc.
+                </p>
+              </div>
+            </CardHeader>
+            <Divider/>
+            <CardBody>
+              <ul className="list-decimal">
+                <li>Performed 28 SQL transformations from Oracle SQL to AWS Athena SQL.</li>
+                <br></br>
+                <li>Worked closely with AWS Glue and AWS Athena Query Editor</li>
+              </ul>
+            </CardBody>
+            <Divider/>
+            <CardFooter>
+              <Link
+                isExternal
+                showAnchorIcon
+                href="https://www.linkedin.com/company/lenaph/"
+              >
+                Visit company on LinkedIn.
+              </Link>
+            </CardFooter>
+          </Card>
+          {/* LENA */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {/* ALTDSI */}
+          <Card className="max-w-full mt-10">
+            <CardHeader className="flex gap-3">
+              <Image
+                alt="nextui logo"
+                height={40}
+                radius="sm"
+                src="/altdsi.png"
+                width={40}
+              />
+              <div className="flex flex-col">
+                <p className="text-md">Research Assistant</p>
+                <p className="text-small text-default-500">
+                    Dr. Andrew L. Tan Data Science Institute
+                </p>
+              </div>
+            </CardHeader>
+            <Divider/>
+            <CardBody>
+              <ul className="list-decimal">
+                <li>...</li>
+                <br></br>
+                <li>...</li>
+              </ul>
+            </CardBody>
+            <Divider/>
+            <CardFooter>
+              <Link
+                isExternal
+                showAnchorIcon
+                href="https://altdsi.dlsu.edu.ph/"
+              >
+                Visit institute website.
+              </Link>
+            </CardFooter>
+          </Card>
+          {/* ALTDSI */}
+        </div>
+        <div id="projects" className="pb-40">Projects</div>
+        <div id="contributions" className="pb-40">Contributions</div>
       </div>
     </main>
   );
