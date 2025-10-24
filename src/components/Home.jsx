666d846f6e74d0514a567726f135af0e08a5e0b0
GenuinoVitto <vittogenuino@gmail.com>
2025-10-25 01:15:15 +0800

    added chakra ui and download resume button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { Card, Heading, Stack, Text, Timeline } from "@chakra-ui/react"
+import { LuCheck, LuPackage, LuShip, LuNotebook, LuLinkedin, LuInstagram } from "react-icons/lu"
 function Home() {
   return (
     <div className="content">
@@ -8,9 +10,9 @@ function Home() {
         </div>
 
         <div className="content-greeting">
-          <h2>Hello, I'm Vitto Genuino</h2>
-          <p>This is the main landing page of the application.</p>
-          <button className="resume-btn">Download Resume</button>
+          <h1 className="greeting">Hello, I'm Vitto</h1>
+          <p className="sub-greeting">Get a copy of my resume to see my credentials</p>
+          <a href="src\assets\Resume - Genuino, Jose Mari Victorio G..pdf" download className="download-btn">Download resume 👨‍💻</a>
         </div>
       </section>
 
@@ -19,19 +21,103 @@ function Home() {
         <p>
           I'm Vitto Genuino, a passionate developer with experience in building web applications.
         </p>
+        <Timeline.Root maxW="600px">
+          <Timeline.Item>
+            <Timeline.Connector>
+              <Timeline.Separator />
+              <Timeline.Indicator>
+                <LuNotebook />
+              </Timeline.Indicator>
+            </Timeline.Connector>
+            <Timeline.Content>
+              <Timeline.Title>Product Shipped</Timeline.Title>
+              <Timeline.Description>13th May 2021</Timeline.Description>
+              <Text textStyle="sm">
+                We shipped your product via <strong>FedEx</strong> and it should
+                arrive within 3-5 business days.
+              </Text>
+            </Timeline.Content>
+          </Timeline.Item>
+
+          <Timeline.Item>
+            <Timeline.Connector>
+              <Timeline.Separator />
+              <Timeline.Indicator>
+                <LuCheck />
+              </Timeline.Indicator>
+            </Timeline.Connector>
+            <Timeline.Content>
+              <Timeline.Title textStyle="sm">Order Confirmed</Timeline.Title>
+              <Timeline.Description>18th May 2021</Timeline.Description>
+            </Timeline.Content>
+          </Timeline.Item>
+
+          <Timeline.Item>
+            <Timeline.Connector>
+              <Timeline.Separator />
+              <Timeline.Indicator>
+                <LuPackage />
+              </Timeline.Indicator>
+            </Timeline.Connector>
+            <Timeline.Content>
+              <Timeline.Title textStyle="sm">Order Delivered</Timeline.Title>
+              <Timeline.Description>20th May 2021, 10:30am</Timeline.Description>
+            </Timeline.Content>
+          </Timeline.Item>
+        </Timeline.Root>
       </section>
 
       <section className="projects">
         <h2>Projects</h2>
-        <ul>
-          <li>Project 1: A web application for managing tasks.</li>
-        </ul>
-        <Link to="/projects">Go to Projects Page</Link>
+        {/* <Stack>
+          <Card.Root size="md">
+            <Card.Header>
+              <Heading size="md"> Card - sm</Heading>
+            </Card.Header>
+            <Card.Body color="fg.muted">
+              This is the card body. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit.
+            </Card.Body>
+          </Card.Root>
+
+          <Card.Root size="md">
+            <Card.Header>
+              <Heading size="md"> Card - md</Heading>
+            </Card.Header>
+            <Card.Body color="fg.muted">
+              This is the card body. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit.
+            </Card.Body>
+          </Card.Root>
+
+          <Card.Root size="md">
+            <Card.Header>
+              <Heading size="md"> Card - lg</Heading>
+            </Card.Header>
+            <Card.Body color="fg.muted">
+              This is the card body. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit.
+            </Card.Body>
+          </Card.Root>
+        </Stack> */}
+        <Link to="/projects">See more</Link>
       </section>
 
+    <hr></hr>
+
       <section className="contact">
-        <h2>Contact Me</h2>
-        <p>If you'd like to get in touch, feel free to reach out via email or social media.</p>
+          <h2>Contact Me</h2>
+          <p>If you'd like to get in touch, feel free to reach out via email or social media.</p>
+          <p>email: vitto.g.genuino@gmail.com</p>
+          <p>mobile: +63 917 867 9172</p>
+        <div className="contact-socials">
+          <a href='https://www.linkedin.com/in/genuinovitto/'>
+            <LuLinkedin size={30} />
+          </a>
+          <a href='https://www.instagram.com/GenuinoVitto/'>
+            <LuInstagram size={30} />
+          </a>
+        </div>    
       </section>
     </div>
   )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,21 @@
+"use client"
+import { Button } from "@chakra-ui/react"
+import { useColorMode } from "@/components/ui/color-mode"
 function Nav() {
+  const { toggleColorMode } = useColorMode()
   return (
     <>
       <nav className="navbar">
         <div className="container">
             <div className="nav-container">
                 <div className="navbar-logo">
-                    <h1>Vitto Genuino</h1>
+                    <h1 className="navbar-icon">Jose Mari Victorio G. Genuino</h1>
                 </div>
                 <div className="navbar-links">
-                  <button>
-                    Download Resume
-                  </button>
+                  {/* <a href="src\assets\Resume - Genuino, Jose Mari Victorio G..pdf" download className="download-btn">Download resume</a> */}
+                  <Button variant="outline" onClick={toggleColorMode}>
+                          Toggle Mode
+                  </Button>  
                     {/* <a href="/">Home</a>
                     <a href="/">Projects</a>
                     <a href="/">About</a>
